d834ebee6b11df1a30b8ea6c7c9d348ffe56d26d
Vitor Silva <silvavitoremanuel50@gmail.com>
2021-12-03 15:26:46 -0300

    Primeira Parte Crud

---
+++ views/header.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="imagem/png" href="/model/imagens/Barber.png" />
+    <link rel="stylesheet" href="/model/css/style.css">
+    <title>Site Barber</title>
+</head>
+<body>
+
+
+
+<div class="container">
+
+    <div>
+        <div class="container-list">
+
+                <li>
+                    <img class="container-img" src="/model/imagens/Azul Moldura.png" alt="">
+                    
+                </li>
+            <ul>
+
+                <li><a href="">PRODUTOS</a></li>
+
+                <li><a href="">AGENDAMENTOS</a></li>
+
+                <li><a href="">SOBRE</a></li>
+
+                <li><a href="">POLITICA PRIVACIDADE</a></li>
+
+            </ul>
+        
+        </div>
+        <div class="border-box"></div>
+    </div>
+
+</div>
+
+<div class="image-body">
+    <img src="/model/imagens/Bar.jpg" alt="">
+</div>
+    
+</body>
+</html>
